b835a6d98843da5dccfdcc01bd4a8c5bb1668178
simone <simonereligi98@gmail.com>
2024-07-19 16:11:43 +0200

    Revert "Revert "Aggiunto controllo sulla lista voti ( len(voti) !=0 )""
    This reverts commit c8f4339b208b3ace2636bef94dd79f8c81df01d1.

--- ElencoAlunni.py
+++ ElencoAlunni.py
@@ -1,8 +1,9 @@
+import random
 studenti_voti = {"Aldo" : [],
                  "Giovanni" : [23, 18, 28],
                  "Giacomo" : [30, 22, 22, 24]}
 media_voti = {}
 for studente, voti in studenti_voti.items():
-    if len(voti) != 0:
+    if len(voti) !=0:
         media_voti[studente] = round(sum(voti) / len(voti), 2)
 print(media_voti)
